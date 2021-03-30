a87b1169d4a5f851b8b472c86e670aefe5494e90
kosmas1991 <kosmasgou@gmail.com>
2021-03-30 14:38:32 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/gr/techzombie/youtube/MainActivity.kt
@@ -0,0 +1,6 @@
+package gr.techzombie.youtube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:developer';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(YoutubeApp());
+}
+
+///
+class YoutubeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Youtube Player IFrame Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        iconTheme: const IconThemeData(color: Colors.deepPurpleAccent),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: YoutubeAppDemo(),
+    );
+  }
+}
+
+///
+class YoutubeAppDemo extends StatefulWidget {
+  @override
+  _YoutubeAppDemoState createState() => _YoutubeAppDemoState();
+}
+
+class _YoutubeAppDemoState extends State<YoutubeAppDemo> {
+  YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: 'IX3DlWkdfEQ',
+      params: const YoutubePlayerParams(
+        playlist: [
+
+        ],
+        startAt: const Duration(minutes: 0, seconds: 0),
+        showControls: true,
+        showFullscreenButton: true,
+        desktopMode: true,
+        privacyEnhanced: true,
+        useHybridComposition: true,
+      ),
+    );
+    _controller.onEnterFullscreen = () {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+      ]);
+      log('Entered Fullscreen');
+    };
+    _controller.onExitFullscreen = () {
+      log('Exited Fullscreen');
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const player = YoutubePlayerIFrame();
+    return YoutubePlayerControllerProvider(
+      // Passing controller to widgets below.
+      controller: _controller,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Youtube Player IFrame'),
+        ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            if (kIsWeb && constraints.maxWidth > 800) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Expanded(child: player),
+                ],
+              );
+            }
+            return ListView(
+              children: [
+                player,
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+}
+
